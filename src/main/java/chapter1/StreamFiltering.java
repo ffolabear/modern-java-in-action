e6baae641671e350ca0cb515f91f8e83bf949814
ffolabear <ffolabear@gmail.com>
2022-11-27 12:09:44 +0900

    chore: 접근제한자 변경

---
+++ src/main/java/chapter1/StreamFiltering.java
@@ -0,0 +1,44 @@
+package chapter1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFiltering {
+
+    public static void main(String[] args) {
+
+        Inventory inventory = new Inventory();
+        List<Apple> appleInventory = inventory.getInventory();
+
+        Util.printList(sequentialStreamFiltering(appleInventory));
+
+        System.out.println();
+
+        Util.printList(parallelStreamFiltering(appleInventory));
+    }
+
+    //순차 처리방식
+    public static List<Apple> sequentialStreamFiltering(List<Apple> appleInventory) {
+        long start = System.currentTimeMillis();
+
+        List<Apple> heavyApple = appleInventory.stream().filter(
+                (Apple a) -> a.getWeight() > 150)
+                .collect(Collectors.toList());
+
+        long end = System.currentTimeMillis();
+        System.out.println("parallelStreamFiltering : " + (end - start)/1000.0);
+        return heavyApple;
+    }
+
+    //병렬 처리방식
+    public static List<Apple> parallelStreamFiltering(List<Apple> appleInventory) {
+        long start = System.currentTimeMillis();
+        List<Apple> heavyApple = appleInventory.parallelStream().filter(
+                (Apple a) -> a.getWeight() > 150)
+                .collect(Collectors.toList());
+        long end = System.currentTimeMillis();
+        System.out.println("parallelStreamFiltering : " + (end - start)/1000.0);
+        return heavyApple;
+    }
+
+}
--- src/main/java/chapter1/Util.java
+++ src/main/java/chapter1/Util.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 public class Util {
 
-    public void printList(List<Apple> sortingResult) {
+    public static void printList(List<Apple> sortingResult) {
         for (Apple apple : sortingResult) {
             System.out.println("apple : " + "weight = " + apple.getWeight() + " / color = " + apple.getColor());
         }
-
     }
 
 }
