e9ab2ae135fcf71f77ab671922665167c003af39
ffolabear <ffolabear@gmail.com>
2022-11-30 19:43:11 +0900

    feat: 챕터2 예제코드 작성

---
+++ src/main/java/chapter2/RequirementsApply.java
@@ -0,0 +1,72 @@
+package chapter2;
+
+import common.Apple;
+import common.Color;
+import common.Inventory;
+import common.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequirementsApply {
+
+    static Inventory inventory = new Inventory();
+
+    public static void main(String[] args) {
+        List<Apple> apples = inventory.getInventory();
+        Util.printList(filterGreenApples(apples));
+        Util.printList(filterApplesByColor(apples, Color.GREEN));
+        Util.printList(filterApples(apples, Color.GREEN, 150, true));
+
+    }
+
+    //첫번째 시도
+    //녹색 사과 필터링
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (Color.GREEN.getColor().equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    //두번째 시도
+    //색을 파라미터화
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    //무게일때도 반영
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, int weight) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > weight) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+
+    //세번째 시도
+    //가능한 모든 속성으로 필터링 -> 형편없는 코드
+    //flag 는 왜 존재하는가?
+    public static List<Apple> filterApples(List<Apple> inventory, Color color, int weight, boolean flag) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ((flag && apple.getColor().equals(color.getColor())) || (!flag && apple.getWeight() > weight)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+}
