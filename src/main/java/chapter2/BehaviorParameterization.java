cafe4ad96d4bf5f88cb11bd991dcc25383b34a21
ffolabear <ffolabear@gmail.com>
2022-12-16 22:52:26 +0900

    chore(BehaviorParameterization): 주석 해제

--- src/main/java/chapter2/BehaviorParameterization.java
+++ src/main/java/chapter2/BehaviorParameterization.java
@@ -4,6 +4,7 @@ import common.Apple;
 import common.Color;
 import common.Inventory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BehaviorParameterization {
@@ -43,14 +44,14 @@ public class BehaviorParameterization {
     }
 
 
-//    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
-//        List<Apple> result = new ArrayList<>();
-//        for (Apple apple : inventory) {
-//            if (p.test(apple)) {
-//                result.add(apple);
-//            }
-//        }
-//        return result;
-//    }
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
 
 }
