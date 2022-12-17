bc98ae59e959b756f21eaeda1dbafc0008e8638a
ffolabear <ffolabear@gmail.com>
2022-12-17 23:55:46 +0900

    feat(AnonymousClass): 익명클래스 활용 예제 코드 작성

--- src/main/java/chapter2/AnonymousClass.java
+++ src/main/java/chapter2/AnonymousClass.java
@@ -4,6 +4,7 @@ import common.Apple;
 import common.Color;
 import common.Inventory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnonymousClass {
@@ -11,7 +12,17 @@ public class AnonymousClass {
     public static void main(String[] args) {
 
         Inventory inventory = new Inventory();
-        List<Apple> apples = inventory.getInventory();
+        List<Apple> appleInventory = inventory.getInventory();
+
+
+        List<Apple> anonymousFilter1 = filteringApples(appleInventory, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return Color.RED.getColor().equals(apple.getColor());
+            }
+        });
+
+        List<Apple> anonymousFilter2 = filteringApples(appleInventory, (Apple apple) -> Color.RED.getColor().equals(apple.getColor()));
 
     }
 
@@ -29,9 +40,16 @@ public class AnonymousClass {
         }
     }
 
-    public class FilteringApples {
-
+    public static List<Apple> filteringApples(List<Apple> inventory, ApplePredicate p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
     }
+
 
     public interface ApplePredicate {
         boolean test(Apple apple);
