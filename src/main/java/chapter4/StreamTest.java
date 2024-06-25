0da10ad74833ba6c78b4171919e49c25d035a0b1
ffolabear <ffolabear@gmail.com>
2024-06-25 20:43:55 +0900

    feat : 챕터 4 예제 추가

---
+++ src/main/java/chapter4/StreamTest.java
@@ -0,0 +1,42 @@
+package chapter4;
+
+import static chapter4.Dish.Type.*;
+
+import chapter4.Dish.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, MEAT),
+                new Dish("beek", false, 700, MEAT),
+                new Dish("chicken", false, 400, MEAT),
+
+                new Dish("french fries", true, 530, OTHER),
+                new Dish("rice", true, 350, OTHER),
+                new Dish("season fruit", true, 120, OTHER),
+
+                new Dish("pizza", true, 550, OTHER),
+                new Dish("prawns", false, 300, FISH),
+                new Dish("salmon", false, 450, FISH)
+        );
+
+        List<String> names = menu.stream()
+                .filter(dish -> {
+                    System.out.println("filtering : " + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("maping : " + dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(names);
+    }
+
+}
