5c1ed2d77cf5fd56812cf0fd67d3d663f266fe5f
ffolabear <ffolabear@gmail.com>
2024-10-10 23:59:09 +0900

    feat : 챕터10 예제 추가

---
+++ src/main/java/chapter10/DSLTest.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public class DSLTest {
+
+    public static void main(String[] args) {
+
+        Order order = new Order();
+
+    }
+
+}
---
+++ src/main/java/chapter10/Trade.java
@@ -0,0 +1,36 @@
+package chapter10;
+
+public class Trade {
+
+    public enum Type {
+        BUY, SELL
+    }
+
+    private Stock stock;
+    private int quantity;
+    private double price;
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
